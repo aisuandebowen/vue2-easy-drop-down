45e95bfdeaf71c0a9e813037d32a7af36cc9a1d8
Boen Chen <58322181+aisuandebowen@users.noreply.github.com>
2023-11-08 16:54:38 +0800

    test: Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,74 @@
-<!--
- * @Author: cbw
- * @Date: 2023-11-06 19:06:42
- * @LastEditors: cbw
- * @LastEditTime: 2023-11-06 19:08:46
- * @Description: 
--->
 <template>
   <div id="app">
-    <DropDown>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
-      <h1>你好</h1>
+    <div class="header">
+      <DropDown>
+        <h1>点击</h1>
+        <template v-slot:overlay>
+          <div class="show"></div>
+        </template>
+      </DropDown>
+    </div>
+    <div class="left">
+      <DropDown>
+        <h1>点击</h1>
+        <template v-slot:overlay>
+          <div class="show"></div>
+        </template>
+      </DropDown>
+    </div>
 
-      <div slot="overlay" class="menu"></div>
-    </DropDown>
+    <div class="content">
+      <DropDown>
+        <h1>点击</h1>
+        <template v-slot:overlay>
+          <div class="show"></div>
+        </template>
+      </DropDown>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-    name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style>
-
-.menu{
-  background-color: rosybrown;
-  width: 100px;
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.header {
   height: 100px;
+  width: 100%;
+  background-color: antiquewhite;
+}
+
+.left {
+  width: 100px;
+  height: calc(100% - 100px);
+  background-color: burlywood;
+}
+
+.content {
+  flex: 1;
+  background-color: rgb(153, 161, 158);
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.show {
+  width: 60px;
+  height: 100px;
+  background-color: #00eeff5b;
 }
 </style>
